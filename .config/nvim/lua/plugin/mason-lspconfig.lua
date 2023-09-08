ac6633f9f88ff3401c7135ce72cfa72ae5fa0aca
cross-ts <ts.kouhei@gmail.com>
2023-09-08 09:57:39 +0900

    Add mason, mason-lspconfig

---
+++ .config/nvim/lua/plugin/mason-lspconfig.lua
@@ -0,0 +1,10 @@
+-- Extension to mason.nvim that makes it easier to use lspconfig with mason.nvim.
+return {
+  'williamboman/mason-lspconfig.nvim',
+  cmd = { 'LspInstall', 'LspUninstall' },
+  dependencies = {
+    'williamboman/mason.nvim',
+    'neovim/nvim-lspconfig',
+  },
+  config = true,
+}
