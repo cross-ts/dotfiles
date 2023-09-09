5a0d8f9c1350c861af830b6474f6c4be237f4faf
cross-ts <ts.kouhei@gmail.com>
2023-09-09 16:16:27 +0900

    Add nvim-treesitter

---
+++ .config/nvim/lua/plugin/nvim-treesitter.lua
@@ -0,0 +1,12 @@
+-- Nvim Treesitter configurations and abstraction layer
+return {
+  'nvim-treesitter/nvim-treesitter',
+  cmd = { 'TSUpdate' },
+  config = function()
+    require('nvim-treesitter.configs').setup({
+      ensure_installed = {
+        'lua',
+      },
+    })
+  end
+}
