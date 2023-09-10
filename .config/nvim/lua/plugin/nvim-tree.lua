e99ada11d51250b6e76c193ddf6c653f3a96bb90
cross-ts <ts.kouhei@gmail.com>
2023-09-10 20:02:21 +0900

    nvim-tree Auto Close

--- .config/nvim/lua/plugin/nvim-tree.lua
+++ .config/nvim/lua/plugin/nvim-tree.lua
@@ -4,7 +4,7 @@ return {
   dependencies = {
     'nvim-tree/nvim-web-devicons',
   },
-  cmd = { 'NvimTreeOpen', "NvimTreeToggle" },
+  cmd = { 'NvimTreeOpen', 'NvimTreeToggle' },
   init = function()
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
@@ -18,6 +18,34 @@ return {
           enable = true,
         }
       }
+    })
+
+    -- AutoClose
+    -- See: https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close
+    local group = 'NvimTreeAutoClose'
+    vim.api.nvim_create_augroup(group, {clear = true})
+    vim.api.nvim_create_autocmd('QuitPre', {
+      group = group,
+      callback = function()
+        local tree_wins = {}
+        local floating_wins = {}
+        local wins = vim.api.nvim_list_wins()
+        for _, w in ipairs(wins) do
+          local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+          if bufname:match('NvimTree_') ~= nil then
+            table.insert(tree_wins, w)
+          end
+          if vim.api.nvim_win_get_config(w).relative ~= '' then
+            table.insert(floating_wins, w)
+          end
+        end
+        if 1 == #wins - #floating_wins - #tree_wins then
+          -- Should quit, so we close all invalid windows.
+          for _, w in ipairs(tree_wins) do
+            vim.api.nvim_win_close(w, true)
+          end
+        end
+      end
     })
   end
 }
