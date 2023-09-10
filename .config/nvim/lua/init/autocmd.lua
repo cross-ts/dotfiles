7be83ab7a4b7a5d890fc72161805b6c9db0e7c37
cross-ts <ts.kouhei@gmail.com>
2023-09-10 20:02:08 +0900

    fix

--- .config/nvim/lua/init/autocmd.lua
+++ .config/nvim/lua/init/autocmd.lua
@@ -4,7 +4,7 @@ do
     vim.api.nvim_create_augroup(group, {clear = true})
     vim.api.nvim_create_autocmd({'TermOpen'}, {
         group = group,
-        callback = function(ev)
+        callback = function()
             vim.cmd('startinsert')
             vim.opt.number = false
             vim.opt.relativenumber = false
