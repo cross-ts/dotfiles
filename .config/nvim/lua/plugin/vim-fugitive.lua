a9a518343a2f19b90183932d76888e66f9d98ed6
cross-ts <ts.kouhei@gmail.com>
2023-09-10 20:24:03 +0900

    Add vim-fugitive

--- .config/nvim/lua/plugin/vim-fugitive.lua
+++ .config/nvim/lua/plugin/vim-fugitive.lua
@@ -1,5 +1,8 @@
 -- fugitive.vim: A Git wrapper so awesome, it should be illegal
 return {
   'tpope/vim-fugitive',
-  cmd = { 'Git' }
+  cmd = { 'Git' },
+  init = function()
+    vim.api.nvim_set_keymap('n', '<leader>git', ':Git ', { noremap = true, silent = true })
+  end,
 }
