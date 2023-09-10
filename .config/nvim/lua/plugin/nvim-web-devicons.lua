9df27f9c7a3819f78df0891b9a44e6ecd649b6e6
cross-ts <ts.kouhei@gmail.com>
2023-09-10 17:08:54 +0900

    Add config

--- .config/nvim/lua/plugin/nvim-web-devicons.lua
+++ .config/nvim/lua/plugin/nvim-web-devicons.lua
@@ -2,4 +2,10 @@
 return {
   'nvim-tree/nvim-web-devicons',
   lazy = true,
+  config = function()
+    require('nvim-web-devicons').setup({
+      default = true,
+      strict = true,
+    })
+  end,
 }
