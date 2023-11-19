4de172f917f8aff083398db3f7c05db4d6a448f8
cross-ts <ts.kouhei@gmail.com>
2023-11-19 18:14:00 +0900

    Add indent file

---
+++ .config/nvim/indent/go.lua
@@ -0,0 +1,9 @@
+-- Follows Go's coding style guide
+-- See:
+-- - https://go.dev/doc/effective_go#formatting
+-- - https://github.com/golang/go/wiki/CodeReviewComments
+-- - https://pkg.go.dev/cmd/gofmt
+vim.opt.expandtab = false
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
---
+++ .config/nvim/indent/php.lua
@@ -0,0 +1,6 @@
+-- Follows PSR-2 coding style guide
+-- See: https://www.php-fig.org/psr/psr-2/
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
