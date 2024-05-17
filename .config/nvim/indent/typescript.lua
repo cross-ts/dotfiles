9814a28ddd6ed527ace02e4d9f23edc7d9728b65
cross-ts <ts.kouhei@gmail.com>
2024-05-17 11:39:54 +0900

    Update typescript.lua

--- .config/nvim/indent/typescript.lua
+++ .config/nvim/indent/typescript.lua
@@ -1,6 +1,6 @@
 -- Follows Microsoft's TypeScript Coding guidlines
 -- See: https://github.com/Microsoft/TypeScript/wiki/Coding-guidelines
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
