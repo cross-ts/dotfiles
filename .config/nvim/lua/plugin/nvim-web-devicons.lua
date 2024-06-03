5a44dd95ddcb280fa30ecae4a78609cf71824368
cross-ts <ts.kouhei@gmail.com>
2024-06-03 11:21:11 +0900

    nvim-web-devicons.lua を更新

--- .config/nvim/lua/plugin/nvim-web-devicons.lua
+++ .config/nvim/lua/plugin/nvim-web-devicons.lua
@@ -1,7 +1,6 @@
 -- lua `fork` of vim-web-devicons for neovim
 return {
   'nvim-tree/nvim-web-devicons',
-  lazy = true,
   config = function()
     require('nvim-web-devicons').setup({
       default = true,
