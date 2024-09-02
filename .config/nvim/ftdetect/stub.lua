7cfd9d2ea3806d4f4975206bd48c8fa4c09d150a
cross-ts <ts.kouhei@gmail.com>
2024-09-03 01:27:31 +0900

    for php ftdetect

--- .config/nvim/ftdetect/stub.lua
+++ .config/nvim/ftdetect/stub.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  pattern = "*.neon",
+  pattern = "*.stub",
   callback = function()
-    vim.bo.filetype = "yaml"
+    vim.bo.filetype = "php"
   end,
 })
