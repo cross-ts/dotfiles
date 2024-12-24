acc5c003afa8c30a28a5c671cc2a5d5cbead24d6
cross-ts <ts.kouhei@gmail.com>
2024-12-25 01:02:06 +0900

    remove white space

--- .config/nvim/lua/plugin/nvim-treesitter.lua
+++ .config/nvim/lua/plugin/nvim-treesitter.lua
@@ -9,7 +9,7 @@ return {
       sync_install = false,
       auto_install = true,
       highlight = { enable = true },
-      indent = { enable = true },  
+      indent = { enable = true },
     })
   end
 }
