492fdddba52c2ef4238e783c0dae53ae4d5c6b29
cross-ts <ts.kouhei@gmail.com>
2024-12-26 13:38:04 +0900

    lazy.nvim管理のバージョンを使用する

--- .config/nvim/lua/lazy/setup/init.lua
+++ .config/nvim/lua/lazy/setup/init.lua
@@ -4,17 +4,22 @@ if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     'git',
     'clone',
-    '--filter=blob:none',
+    '--single-branch',
     'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
     lazypath,
   })
+
+  local f = io.open(vim.fn.stdpath('config') .. '/lazy-lock.json', 'r')
+  if f then
+    local data = f:read('*a')
+    local lock = vim.json.decode(data)
+    vim.fn.system { 'git', '-C', lazypath, 'checkout', lock['lazy.nvim'].commit }
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
-local opts = {
+require('lazy').setup('plugin', {
   defaults = {
     lazy = true,
   },
-}
-require('lazy').setup('plugin', opts)
+})
