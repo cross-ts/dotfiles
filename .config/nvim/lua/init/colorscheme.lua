74b63bd6dd20974a9b8655e305676b793aaf9cf4
cross-ts <ts.kouhei@gmail.com>
2025-01-02 14:24:28 +0900

    neovimも透けさせる

--- .config/nvim/lua/init/colorscheme.lua
+++ .config/nvim/lua/init/colorscheme.lua
@@ -1,3 +1,8 @@
 -- colorscheme
 vim.cmd.colorscheme('habamax') -- Use built-in colorscheme
+
 vim.api.nvim_set_hl(0, 'VertSplit', { bg = 'NONE' })
+vim.api.nvim_set_hl(0, 'Normal', { ctermbg = 'NONE' })
+vim.api.nvim_set_hl(0, 'NonText', { ctermbg = 'NONE' })
+vim.api.nvim_set_hl(0, 'SpecialKey', { ctermbg = 'NONE' })
+vim.api.nvim_set_hl(0, 'EndOfBuffer', { ctermbg = 'NONE' })
